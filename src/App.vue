2ca36a77c31b6d0cc081115bba9c9f79c5760dde
vuvinhhien <hien.vu@hdwebsoft.io>
2022-04-24 14:58:00 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+    <div><router-view /></div>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  width: 100%;
+}
+
+nav {
+  padding: 30px;
+  width: 20%;
+  a {
+    display: block;
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+
+div {
+  width: 80%;
+}
+</style>
