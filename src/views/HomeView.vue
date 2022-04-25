0f0cbf7c971d0a84939f5abf0b89c093ee74afa1
vuvinhhien <hien.vu@hdwebsoft.io>
2022-04-25 13:52:23 +0700

    add vuetify

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
+<template>
+  <hello-world />
+</template>
+
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue";
+import { defineComponent } from 'vue';
+
+// Components
+import HelloWorld from '../components/HelloWorld.vue';
 
 export default defineComponent({
-  name: "HomeView",
+  name: 'HomeView',
+
   components: {
     HelloWorld,
   },
 });
 </script>
-
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
-</template>
